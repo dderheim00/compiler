9491c269b229eb0b56148df0c04c0bdd22faca63
Daniel Derheim <56704555+dderheim00@users.noreply.github.com>
2023-03-06 22:45:06 -0600

    Update compiler.cpp

--- compiler.cpp
+++ compiler.cpp
@@ -1,8 +1,3 @@
-    /*
-        
-    */
-
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,68 +7,116 @@
 using namespace std;
 
 vector<vector<string> > csvData;
+vector<string> csvRW;
 
-void readInput(string csvFile){
+void readInput(string csvFile, string kind){
     ifstream csvInputFile(csvFile);
     if(csvInputFile.is_open()) {
-        string line;
-        while(getline(csvInputFile, line)) {
+        string temp;
+        while(getline(csvInputFile, temp)) {
             vector<string> row;
-            stringstream ss(line);
-            string field;
-            while(getline(ss, field, ',')){
-                row.push_back(field);
+            stringstream strsrm(temp);
+            string cell;
+            while(getline(strsrm, cell, ',')){
+                row.push_back(cell);
             }
-            csvData.push_back(row);
+            if(kind == "Data"){
+                csvData.push_back(row);
+            }
+            else if(kind == "Reserved"){
+                for (int i = 0; i < row.size(); i++) {
+                    string j = row[i];
+                    csvRW.push_back(j);
+                }
+            }
+            else{
+                exit(1);
+            }
         }
     }
     csvInputFile.close();
 }
 
+
+
 vector<pair<string, string> > tokenizer(string input){
     vector<pair<string, string> > tokens;
     int column;
-    string tokenValue;
-    string number;
-    int j, k, l, m, n, p, q, r;
+    string token;
+    string num;
 
     for(int i = 0; i < input.size(); i++){
-        char ch = input[i];
-        switch(ch) {
+        char currentChar = input[i];
+        switch(currentChar) {
             default:
                 break;
 
             case 'a'...'z':
             case 'A'...'Z':
                 column = 1;
-                tokenValue = "";
-                tokenValue = tokenValue + ch;
+                int j;
+                token = "";
+                token = token + currentChar;
                 j = i + 1;
                 while(j < input.size() && (isalpha(input[j]) || isdigit(input[j]))){
                     char nextChar = input[j];
-                    tokenValue = tokenValue + nextChar;
+                    token = token + nextChar;
                     j++;
                 }
-                tokens.push_back(pair<string, string>(tokenValue, csvData[3][column]));
+                if(token == csvRW[1]){
+                    tokens.push_back(pair<string, string>(token, "$CONST"));
+                }
+                else if(token == csvRW[2]){
+                    tokens.push_back(pair<string, string>(token, "$IF"));
+                }
+                else if(token == csvRW[3]){
+                    tokens.push_back(pair<string, string>(token, "$VAR"));
+                }
+                else if(token == csvRW[4]){
+                    tokens.push_back(pair<string, string>(token, "$THEN"));
+                }
+                else if(token == csvRW[5]){
+                    tokens.push_back(pair<string, string>(token, "$PROCEDURE"));
+                }
+                else if(token == csvRW[6]){
+                    tokens.push_back(pair<string, string>(token, "$WHILE"));
+                }
+                else if(token == csvRW[7]){
+                    tokens.push_back(pair<string, string>(token, "$CALL"));
+                }
+                else if(token == csvRW[8]){
+                    tokens.push_back(pair<string, string>(token, "$DO"));
+                }
+                else if(token == csvRW[9]){
+                    tokens.push_back(pair<string, string>(token, "$ODD"));
+                }
+                else if(token == csvRW[10]){
+                    tokens.push_back(pair<string, string>(token, "$CLASS"));
+                }
+                else {
+                    tokens.push_back(pair<string, string>(token, csvData[3][column]));
+                }
                 i = j - 1;
                 break;
 
             case '0'...'9':
                 column = 2;
-                number = "";
-                number = number + ch;
+                int k;
+                num = "";
+                num = num + currentChar;
                 k = i + 1;
                 while(k < input.size() && input[k] >= '0' && input[k] <= '9'){
                     char nextChar = input[k];
-                    number = number + nextChar;
+                    num = num + nextChar;
                     k++;
                 }
-                tokens.push_back(pair<string, string>(number, csvData[4][column]));
+                tokens.push_back(pair<string, string>(num, csvData[4][column]));
                 i = k - 1;
                 break;
 
             case '=':
                 column = 3;
+                int l;
                 l = i + 1;
                 if(l < input.size() && input[l] == '='){
                     tokens.push_back(pair<string, string>("==", csvData[16][column]));
@@ -106,6 +149,7 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '*':
                 column = 8;
+                int m;
                 m = i + 1;
                 if(m < input.size() && input[m] == '/'){
                     tokens.push_back(pair<string, string>("*/", csvData[23][column]));
@@ -118,6 +162,7 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '/':
                 column = 9;
+                int n;
                 n = i + 1;
                 if(n < input.size() && input[n] == '*'){
                     tokens.push_back(pair<string, string>("/*", csvData[22][column]));
@@ -140,6 +185,7 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '!':
                 column = 12;
+                int p;
                 p = i + 1;
                 if(p < input.size() && input[p] == '='){
                     tokens.push_back(pair<string, string>("<=", csvData[17][column]));
@@ -152,7 +198,8 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '<':
                 column = 13;
-                q = i + 1;
+                int q;
+                j = i + 1;
                 if(q < input.size() && input[q] == '='){
                     tokens.push_back(pair<string, string>("<=", csvData[14][column]));
                     i = q; //skips the second symbol
@@ -164,6 +211,7 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '>':
                 column = 14;
+                int r;
                 r = i + 1;
                 if(r < input.size() && input[r] == '='){
                     tokens.push_back(pair<string, string>(">=", csvData[15][column]));
@@ -176,12 +224,12 @@ vector<pair<string, string> > tokenizer(string input){
 
             case '{':
                 column = 15;
-                tokens.push_back(pair<string, string>("(", csvData[20][column]));
+                tokens.push_back(pair<string, string>("{", csvData[20][column]));
                 break;
 
             case '}':
                 column = 16;
-                tokens.push_back(pair<string, string>("(", csvData[21][column]));
+                tokens.push_back(pair<string, string>("}", csvData[21][column]));
                 break;
         }
     }
@@ -189,20 +237,24 @@ vector<pair<string, string> > tokenizer(string input){
     return tokens;
 }
 
+
 int main(){
-    readInput("CompilerTable.csv");
-    ifstream inputFile("inputfile.txt");
-    string input; //store input file into this string
+    readInput("CompilerTable.csv", "Data");
+    readInput("ReservedWords.csv", "Reserved");
+    ifstream inputFile("InputFile.txt");
+    string input; //store input file here
     if (inputFile.is_open()){
         getline(inputFile, input, '\0');
     }
     inputFile.close();
+    
+    vector<pair<string, string> > tokensList;
+    pair<string, string> tokenPair;
+    tokensList = tokenizer(input);
 
-    vector<pair<string, string> > tokens = tokenizer(input);
-
-    for (int i = 0; i < tokens.size(); i++){
-        pair<string, string> token = tokens[i];
-        cout << token.first << " : " << token.second << endl;
+    for (int i = 0; i < tokensList.size(); i++){
+        tokenPair = tokensList[i];
+        cout << tokenPair.first << " : " << tokenPair.second << endl;
     }
 
     return 0;
